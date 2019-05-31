1b334b5f74c7fef4af3a1fcfad41cdf40db90548
delilah14 <dilluxdes@gmail.com>
2019-05-31 15:48:48 +0300

    new gitignore file added

--- app/src/main/java/com/iyoa/cleanaddis/data/news/Article.kt
+++ app/src/main/java/com/iyoa/cleanaddis/data/news/Article.kt
@@ -15,12 +15,9 @@ import java.util.*
 @Entity(tableName = "article")
 data class Article(
     @PrimaryKey
-@ColumnInfo(name="uuid")
-<<<<<<< HEAD
-val uuid: Long,
-=======
-val uuid: Long ,
->>>>>>> 61b9985b276e148cf22369ad1bfdeb5b11542bc6
+    @ColumnInfo(name="uuid")
+    val uuid: Long,
+
     @ColumnInfo(name="title") val title:String,
     @ColumnInfo(name="media_uuid")val media_uuid: String,
     @ColumnInfo(name = "text") val text:String,
