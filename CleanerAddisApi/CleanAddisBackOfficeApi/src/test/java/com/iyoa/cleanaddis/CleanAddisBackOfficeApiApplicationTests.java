2c30ebf271d6426198e0d0baa3050bdb871669c3
delilah14 <dilluxdes@gmail.com>
2019-06-03 11:29:46 +0300

    Cleaner addis api added

---
+++ CleanerAddisApi/CleanAddisBackOfficeApi/src/test/java/com/iyoa/cleanaddis/CleanAddisBackOfficeApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iyoa.cleanaddis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CleanAddisBackOfficeApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
