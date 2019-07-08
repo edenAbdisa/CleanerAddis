111e82d34fa0db841d352435441999620334f223
delilah14 <dilluxdes@gmail.com>
2019-07-08 05:53:38 +0300

    revert

--- app/src/main/java/com/iyoa/cleanaddis/retrofit/ArticleServiceImpl.kt
+++ app/src/main/java/com/iyoa/cleanaddis/retrofit/ArticleServiceImpl.kt
@@ -1,6 +1,11 @@
 package com.iyoa.cleanaddis.retrofit
 
+<<<<<<< HEAD:app/src/main/java/com/iyoa/cleanaddis/retrofit/ArticleServiceImpl.kt
 import com.iyoa.cleanaddis.utility.BASE_URL_NEW
+=======
+import com.iyoa.cleanaddis.retrofitEden.FriendService
+import com.iyoa.cleanaddis.utility.BASE_URL
+>>>>>>> parent of 259b9a5... Article/News Feature: repository,retrofit endpoints modified:app/src/main/java/com/iyoa/cleanaddis/retrofitDelilah/ArticleServiceImpl.kt
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -9,7 +14,7 @@ class ArticleServiceImpl {
 
     fun getArticleServiceImpl(): ArticleService {
         val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL_NEW)
+            .baseUrl(BASE_URL)
             .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
--- app/src/main/java/com/iyoa/cleanaddis/utility/RetrofitBaseUrl.kt
+++ app/src/main/java/com/iyoa/cleanaddis/utility/RetrofitBaseUrl.kt
@@ -1,8 +1,14 @@
 package com.iyoa.cleanaddis.utility
 
+<<<<<<< HEAD
 
 
 val BASE_URL_NEW:String ="http://192.168.1.107:7990"
 
+=======
+<<<<<<< HEAD
+val BASE_URL:String ="http://192.168.1.1:7990"
+=======
+>>>>>>> parent of 259b9a5... Article/News Feature: repository,retrofit endpoints modified
 val BASE_URL:String ="http://192.168.0.103:7990"
-
+>>>>>>> bf10166a34dd66ddfdf1595d95dcc3c76a034543
