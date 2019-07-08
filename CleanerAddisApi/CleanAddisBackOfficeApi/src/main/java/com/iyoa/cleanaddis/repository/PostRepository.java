310af87f753bcf5158f3e0385e7c4d87415eb92e
edenAbdisa <eden.abdisa@yahoo.com>
2019-07-08 06:22:43 +0300

    API adjusted

--- CleanerAddisApi/CleanAddisBackOfficeApi/src/main/java/com/iyoa/cleanaddis/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iyoa.cleanaddis.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.iyoa.cleanaddis.data.Post;
-
-public interface PostRepository extends JpaRepository<Post, Long>{
-
-}
